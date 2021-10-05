eac682b0fd000729e600a25b97a424138acb6491
Adam-E-B <61750702+Adam-E-B@users.noreply.github.com>
2021-10-05 18:07:28 -0400

    Add player character movement rotation and simple jump ability
    The player character now rotates to where they are going and can perform a rudimentary jump with some flag variables for use with features added later. Fixed the vector used for walking not being consistent when moving diagonally in relative to the horizontal world axes.

--- CapstoneGame/Assets/Scripts/Runtime/CharacterController_Player.cs
+++ CapstoneGame/Assets/Scripts/Runtime/CharacterController_Player.cs
@@ -12,11 +12,14 @@ public class CharacterController_Player : MonoBehaviour
     Ray groundRay;
     RaycastHit groundRayHit;
     public Transform groundRayBase;
+
     // Layer masks involved in character movement
     LayerMask floorLayer;
 
+    // Variables for movement control factors
     Vector3 moveIntent, moveVector;
-    public float moveSpeed = 1;
+    public float moveSpeed = 1, jumpPower = 2;
+    bool isGrounded = false, isJumping = false;
 
     // Start is called before the first frame update
     void Start()
@@ -29,41 +32,75 @@ public class CharacterController_Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Check player input
+        // Check player inputs
         moveIntent = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if(Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
     }
 
     void FixedUpdate()
     {
         // Ground ray is drawn a little bit above player character's foot
         groundRay = new Ray(groundRayBase.position + Vector3.up * 0.5f, Vector3.down);
-
+        
         // Calculate the angle of the normal that the player is standing on (angle rounded to nearest significant digit; for use in later functionality)
         float groundAngle = Mathf.Round(Vector3.Angle(groundRayHit.normal, groundRay.direction));
 
         // Check if ground collision is below
-        if (Physics.Raycast(groundRay, out groundRayHit, 1f, 1 << floorLayer, QueryTriggerInteraction.Ignore))
+        // Added conditional so ground collision is only checked when moving down (velocity-based solution might be better later)
+        if (Physics.Raycast(groundRay, out groundRayHit, 1f, 1 << floorLayer, QueryTriggerInteraction.Ignore) && rb.velocity.y <= 0)
         {
+            // Set grounded flag on
+            isGrounded = true;
+
+            // Readout for angle of normal that is currently stood on
             Debug.Log("Hit!" + ", " + groundAngle % 60 + ", " + groundAngle);
+            
             // Move character by distance of ground collision so that they are standing on top of the ground collision point
             rb.position = new Vector3(transform.position.x, groundRayHit.point.y + 1, transform.position.z);
+            
+            // Draw red line where ground detection is being checked (collision has already happened)
             Debug.DrawRay(groundRayBase.position + Vector3.up * 0.5f, groundRay.direction, Color.red);
 
             // Movement vector is reset
             moveVector = Vector3.zero;
 
             // Player's movement intent is added
-            moveVector += moveIntent * moveSpeed;
-            
+            moveVector += moveIntent.normalized * moveSpeed;
+
+            // Update character's facing direction
+            if (moveIntent.magnitude > 0)
+            {
+                rb.MoveRotation(Quaternion.Euler(new Vector3(0, Mathf.Rad2Deg * Mathf.Atan2(rb.velocity.x, rb.velocity.z), 0)));
+            }
+
+            // Rudimentary jumping (jumping, in this case, is additive onto current movement velocity)
+            if (isJumping)
+            {
+                print("Jump!");
+                moveVector += transform.up * jumpPower;
+                isJumping = false;
+            }
         }
         else
         {
+            // Set grounded flag off
+            isGrounded = false;
+            
+            // Set jumping variable to false to prevent complete jump buffering for after landing (partial jump buffering might be wanted later)
+            isJumping = false;
+
             // Add gravity factor when not standing on a surface
             moveVector += Vector3.down * gravityFac * Time.deltaTime;
+
+            // Draw red line where ground detection is being checked (collision hasn't happened yet)
             Debug.DrawRay(groundRayBase.position + Vector3.up * 0.5f, groundRay.direction, Color.green);
-
-
         }
+
+        // Draw current rotation direction
+        Debug.DrawRay(groundRayBase.position, transform.forward, Color.blue);
 
         // Final movement calculation
         rb.MovePosition(rb.position + moveVector * Time.fixedDeltaTime);
