63fd0c866e6dcbb6388a0707105f8c89aaaa0403
kc971201 <kc971201@gmail.com>
2021-11-05 19:14:18 -0400

    Look at in Doubt works for now

--- CapstoneGame/Assets/DoubtState.cs
+++ CapstoneGame/Assets/DoubtState.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class DoubtState : FSMState
 {
     public GameObject Player;
+    public GameObject MyHolder;
     void Awake()
     {
         stateID = StateID.Doubt;
         AddTransition(Transition.IntoChasing, StateID.Chasing);
         AddTransition(Transition.IntoIdeal, StateID.Ideal);
         Player = GameObject.Find("Player");
+      
+
 
     }
 
@@ -35,10 +38,10 @@ public class DoubtState : FSMState
 
     public override void Reason()
     {
-        if (GetComponentInParent<Attention>().attentionValue>=30)
-        {
-            manager.Fsm.PerformTransition(Transition.IntoChasing);
-        }
+        //if (GetComponentInParent<Attention>().attentionValue>=30)
+        //{
+        //    manager.Fsm.PerformTransition(Transition.IntoChasing);
+        //}
 
         if (GetComponentInParent<Attention>().attentionValue == 0)
         {
@@ -48,7 +51,9 @@ public class DoubtState : FSMState
 
     public override void Act()
     {
-        transform.LookAt(Player.transform);
+        
+        transform.parent.LookAt(Player.transform.position);
+        Debug.Log("Looking works");
 
     }
 }
