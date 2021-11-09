3a93b4c658a200f71436febeb90ea005824e3a00
LPPRRR <wangzuo@sheridancollege.ca>
2021-11-09 22:29:42 +0800

    Adjusted accuracy

--- CapstoneGame/Assets/Rhythm Mechanics/scripts/BeatScroller.cs
+++ CapstoneGame/Assets/Rhythm Mechanics/scripts/BeatScroller.cs
@@ -7,30 +7,33 @@ public class BeatScroller : MonoBehaviour
     // Start is called before the first frame update
 
     public float beatTempo;
+    public AudioSource level1Music;
 
     public bool hasStarted;
 
     void Start()
     {
         beatTempo = beatTempo / 60f;
+        level1Music = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-      //  if (!hasStarted)
-      //  {
-      //      if (Input.anyKeyDown)
-      //      {
-      //          hasStarted = true;
-      //      }
-      //  }
-      //  else
-      //  {
-      //      transform.position -= new Vector3(0f, beatTempo * Time.deltaTime, 0f);
-       // }
+        if (!hasStarted)
+        {
+            if (Input.anyKeyDown)
+            {
+                hasStarted = true;
+                level1Music.Play();
+            }
+        }
+        else
+        {
+            transform.position -= new Vector3(0f, beatTempo * Time.deltaTime, 0f);
+        }
 
-        transform.position -= new Vector3(0f, beatTempo * Time.deltaTime, 0f);
+        //transform.position -= new Vector3(0f, beatTempo * Time.deltaTime, 0f);
     }
 }
