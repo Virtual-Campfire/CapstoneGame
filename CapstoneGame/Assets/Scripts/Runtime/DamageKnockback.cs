7fedc5846ce71d366dc661b95d54ede8525f3cb4
Adam-E-B <61750702+Adam-E-B@users.noreply.github.com>
2021-11-23 10:09:36 -0500

    Add health bars and minor visual changes and tweak test level

--- CapstoneGame/Assets/Scripts/Runtime/DamageKnockback.cs
+++ CapstoneGame/Assets/Scripts/Runtime/DamageKnockback.cs
@@ -3,21 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Adam B.
-// Applies damage and knockback when called from other scripts
+// Applies damage and knockback when called from other scripts and manages visual health bar, if one is present
 public class DamageKnockback : MonoBehaviour
 {
     [Tooltip("Do not enable if death is handled by other script(s).")]
     public bool handleDeath = false;
 
-    public float currentHealth = 1, maxHealth = 1;
+    public float currentHealth = 1, maxHealth = 1, invulnTime = 1;
 
     LayerMask targetLayer;
     float invulnerableUntil;
 
+    [SerializeField]
+    GameObject healthBar;
+    public bool showHealthBar = false;
+    Vector3 healthBarSize;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        // If a health bar exists, record and set initial parameters
+        if (healthBar != null)
+        {
+            healthBarSize = healthBar.transform.localScale;
+            healthBar.SetActive(false);
+        }
     }
 
     // This is meant for testing without full effects
@@ -36,7 +46,9 @@ public class DamageKnockback : MonoBehaviour
         if (Time.fixedTime > invulnerableUntil)
         {
             print("Ouch++!");
-            invulnerableUntil = Time.fixedTime + 1;
+
+            // Set invulnerability time
+            invulnerableUntil = Time.fixedTime + invulnTime;
 
             // Clip knockback Y axis to object's axis
             sourcePosition = new Vector3(sourcePosition.x, transform.position.y, sourcePosition.z);
@@ -46,6 +58,32 @@ public class DamageKnockback : MonoBehaviour
 
             // Apply damage
             currentHealth -= damage;
+
+            // Cap health at 0
+            if (currentHealth < 0)
+            {
+                currentHealth = 0;
+            }
+
+            // Adjust health bar size based on current compared to total
+            if (healthBar != null)
+            {
+                print("HP scale set!");
+                healthBar.transform.localScale = new Vector3(healthBarSize.x / (1 / (currentHealth / maxHealth)), healthBarSize.y, healthBarSize.z);
+            }
+
+            // Show health bar if damaged or overhealed
+            if (healthBar != null)
+            {
+                if (currentHealth != maxHealth && showHealthBar)
+                {
+                    healthBar.SetActive(true);
+                }
+                else
+                {
+                    healthBar.SetActive(false);
+                }
+            }
         }
 
         // If this script overrides death check
---
+++ CapstoneGame/Assets/Scripts/Runtime/FaceCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Adam B.
+// Makes object align itself with camera rotation (good for health bars)
+public class FaceCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Camera.main.transform.rotation;
+
+        // Set 90 degree rotational offset
+        transform.rotation = Quaternion.Euler(transform.rotation.x + 90, transform.rotation.y, transform.rotation.z);
+    }
+}
