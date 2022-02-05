c3b217d2a3acebcaf94216642805fa5808474e96
vuksolujic <80364541+vuksolujic@users.noreply.github.com>
2022-02-04 20:41:14 -0500

    Magic/Ability Bar
    Added a new magic and ability bar and its functionality.

---
+++ CapstoneGame/Assets/Vuk's Assets/Scripts/MagicBarScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MagicBarScript : MonoBehaviour
+{
+
+    public Slider slider;
+    
+    public void SetMaxMagic(int magic)
+    {
+        slider.maxValue = magic;
+        slider.value = magic;
+    }
+
+    public void SetMagic(int magic)
+    {
+        slider.value = magic;
+    }
+}
