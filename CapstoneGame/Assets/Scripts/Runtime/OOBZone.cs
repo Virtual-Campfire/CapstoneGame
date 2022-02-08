b07149622b4dda06be8d9d37cb18211e138f1012
kc971201 <kc971201@gmail.com>
2022-02-08 03:59:04 -0500

    Fix knowing pitfall bug and put the audio back

--- CapstoneGame/Assets/Scripts/Runtime/OOBZone.cs
+++ CapstoneGame/Assets/Scripts/Runtime/OOBZone.cs
@@ -41,5 +41,41 @@ public class OOBZone : MonoBehaviour
             // Destroy any other object so zone doesn't get obstructed by unintended entities (certain tagged objects are allowed in case we want obstructions used for puzzles)
             Destroy(other);
         }
+
+        if (other.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+        }
     }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    // Check if entity inside zone has a health system
+    //    if (other.GetComponent<DamageKnockback>())
+    //    {
+    //        // Call player to respawn if this is an inescapable out-of-bounds
+    //        if (inescapable)
+    //        {
+    //            if (other.GetComponent<EnemyState>())
+    //            {
+    //                other.GetComponent<DamageKnockback>().ApplyDamage(1000);
+    //                Debug.Log("Enemy destroyed by pitfall.");
+    //            }
+
+    //            if (other.GetComponent<CharacterController_Player>())
+    //            {
+    //                other.GetComponent<CharacterController_Player>().TeleportToCheckpoint();
+    //            }
+    //        }
+
+    //        // Damage entities with health in the zone
+    //        other.GetComponent<DamageKnockback>().ApplyDamage(damage);
+    //    }
+    //    else if (other.tag != "DoNotDestroy")
+    //    {
+    //        // Destroy any other object so zone doesn't get obstructed by unintended entities (certain tagged objects are allowed in case we want obstructions used for puzzles)
+    //        Destroy(other);
+    //    }
+    //}
+
 }
