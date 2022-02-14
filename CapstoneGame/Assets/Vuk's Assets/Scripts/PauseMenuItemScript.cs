6a5e217c8409361bb895cf67fa855a2d1143642d
vuksolujic <80364541+vuksolujic@users.noreply.github.com>
2022-02-13 22:52:11 -0500

    Designed and Created the pause menu
    I have designed the UI and implemented the paused menu into Gameplay UI. Both the assets and code are included.

---
+++ CapstoneGame/Assets/Vuk's Assets/Scripts/PauseMenuItemScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenuItemScript : MonoBehaviour
+{
+    public Color hoverColor;
+    public Color baseColor;
+    public Image background;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        background.color = baseColor;
+    }
+
+    public void Select()
+    {
+        background.color = hoverColor;
+    }
+
+    public void DeSelect()
+    {
+        background.color = baseColor;
+    }
+}
