c89120f67d12478b7cc458653b7032e582ba4be0
Adam-E-B <61750702+Adam-E-B@users.noreply.github.com>
2022-02-20 17:26:53 -0500

    Add camera modifier zone near puzzle door
    Added a trigger zone that modifies camera position and rotation and placed one in the level in front of the puzzle door. Moved puzzle pieces so that the camera can fit all important objects in frame when the camera is at the modified angle and so that the player will pick up the first statue before they touch the statue bases at the door (this can be changed back later if the puzzle flows better with the old arrangement).
    
    There is a known issue where the angle of view shown in the camera modifier zone does not match the angle applied during gameplay.

---
+++ CapstoneGame/Assets/Scripts/Runtime/CameraModifier.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Adam B.
+// This script moves the player's camera by changing its target location and angle
+public class CameraModifier : MonoBehaviour
+{
+    GameObject player, cam, oldCamNode;
+
+    [SerializeField]
+    GameObject newCamNode;
+
+    void Awake()
+    {
+        player = GameObject.Find("Player");
+        cam = GameObject.Find("Camera Base");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            // Save the camera's previous target
+            oldCamNode = cam.GetComponent<LerpToTarget>().target.gameObject;
+
+            // Set the main camera's target to this script's camera node
+            cam.GetComponent<LerpToTarget>().target = newCamNode.transform;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            // Set the main camera's target to its previous target
+            cam.GetComponent<LerpToTarget>().target = oldCamNode.transform;
+        }
+    }
+}
--- CapstoneGame/Assets/Scripts/Runtime/LerpToTarget.cs
+++ CapstoneGame/Assets/Scripts/Runtime/LerpToTarget.cs
@@ -16,26 +16,34 @@ public class LerpToTarget : MonoBehaviour
 
     // Camera's target transform, speed, and current movement setting
     public Transform target;
-    public float speed = 1;
+    public float speed = 1, rotSpeed = 2;
     public CameraMoveType mode = CameraMoveType.camInstant;
 
     // Update is called once per frame
     void Update()
     {
+        Quaternion targetAngle = target.localRotation;
+
         if (mode == CameraMoveType.camHard)
         {
             // Smooth follow, with immediate once target is reached
             transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            // Smooth rotation
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetAngle, Time.deltaTime * rotSpeed);
         }
         else if (mode == CameraMoveType.camSoft)
         {
             // Smooth follow, with slow approach to target (reaches target increasingly slower)
             transform.position += (target.position - transform.position) * speed * Time.deltaTime;
+            // Smooth rotation
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetAngle, Time.deltaTime * rotSpeed);
         }
         else
         {
             // Rigid follow, no smoothing on the camera's part
             transform.position = target.position;
+            // Rigid rotation
+            transform.rotation = targetAngle;
         }
     }
 }
