5f468cf9078c1d26715287072516142fd6e1b59b
Adam-E-B <61750702+Adam-E-B@users.noreply.github.com>
2022-02-21 16:45:55 -0500

    Add context-sensitive movement tutorial
    Extended the versatility of the TriggerTutorial script so that it can also activate after a delay if the player remains in the trigger area. The newly-added movement tutorial uses this new code to activate the tutorial only if the player doesn't leave the beach after a set period of time.

--- CapstoneGame/Assets/Scripts/Runtime/TriggerTutorial.cs
+++ CapstoneGame/Assets/Scripts/Runtime/TriggerTutorial.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 // Adam B.
 // This script triggers a tutorial sequence script when players enter the game object's trigger
+// Can activate tutorials instantly, or after a time is spent in the trigger area (ex. for context-sensitive tutorials)
 public class TriggerTutorial : MonoBehaviour
 {
     [SerializeField]
@@ -11,17 +12,57 @@ public class TriggerTutorial : MonoBehaviour
 
     GameObject player;
 
+    // Time player has to be in the trigger area before the tutorial appears
+    [SerializeField]
+    float waitTime = 0, enterTime;
+    
+    // Whether has stayed in the trigger area before
+    bool entered = false, cancelled = false;
+
     void Awake()
     {
         player = GameObject.Find("Player");
     }
 
-    void OnTriggerEnter(Collider other)
+    void Update()
     {
-        // If the player enters the trigger, start the tutorial
-        if (other.gameObject == player)
+        // Activate tutorial sequence if player has not exited the trigger area by the waiting time
+        if (Time.time >= enterTime + waitTime && entered && !cancelled)
         {
             tutorial.RestartTutorial();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // If the player enters the trigger
+        if (other.gameObject == player)
+        {
+            // Instantly start the tutorial if the waiting time is 0
+            if (waitTime == 0)
+            {
+                tutorial.RestartTutorial();
+
+                return;
+            }
+
+            // If this is the player's first entry into the trigger area
+            if (!entered)
+            {
+                entered = true;
+                // Save time first entered into trigger zone
+                enterTime = Time.time;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // If the player exits the trigger
+        if (other.gameObject == player)
+        {
+            // Set tutorial to not show up after the waiting time (in other words, the player doesn't need the hint(s) anymore)
+            cancelled = true;
         }
     }
 }
