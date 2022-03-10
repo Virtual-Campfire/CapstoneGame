4e36e21bb30fd6b109d33d0af4b0a13d8f238c7e
kc971201 <kc971201@gmail.com>
2022-03-09 21:06:34 -0500

    Most Bug Fix... LightIn Conflict remains

--- CapstoneGame/Assets/DoorTurning.cs
+++ CapstoneGame/Assets/DoorTurning.cs
@@ -20,9 +20,7 @@ public class DoorTurning : MonoBehaviour
     void Update()
     {
         Vector3 angles = transform.rotation.eulerAngles;
-        Debug.Log(angles);
 
-        Debug.Log(angles);
         if (OpenTheDoor)
         {
             transform.Rotate(Rotation * Time.deltaTime * Speed);
