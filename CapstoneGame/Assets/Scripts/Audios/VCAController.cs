d4511e25ced9b9a881abaf4e4b5a1e5b8b4580a2
LPPRRR <wangzuo@sheridancollege.ca>
2022-03-28 17:04:57 +0800

    Add VCAController Script

--- CapstoneGame/Assets/Scripts/Audios/VCAController.cs
+++ CapstoneGame/Assets/Scripts/Audios/VCAController.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class VCAController : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private FMOD.Studio.VCA VcaController;
+    public string VcaName;
+
+    FMOD.Studio.EventInstance _event;
+
     void Start()
     {
-        
+        VcaController = FMODUnity.RuntimeManager.GetVCA("vca:/" + VcaName);
+
     }
 
     // Update is called once per frame
