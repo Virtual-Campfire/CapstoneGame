0ffd6d0aed58294b49125d7145fc9be5df7ff2d8
Adam-E-B <61750702+Adam-E-B@users.noreply.github.com>
2022-04-14 20:47:59 -0400

    Replace placeholder goal w/ violin, fix Bob/Rotate
    The new violin model replaces the golden ukulele placeholder for the win condition pickup. Fixed a bug where the Bob/Rotate script was calculating the height of each bob with the variable for rotations per second instead of modifying the rotation speed like it was supposed to. Also made the violin rotate a bit slower so that players can get a proper look at its details.

--- CapstoneGame/Assets/Scripts/Runtime/BobRotate.cs
+++ CapstoneGame/Assets/Scripts/Runtime/BobRotate.cs
@@ -23,9 +23,9 @@ public class BobRotate : MonoBehaviour
     void Update()
     {
         // Rotate the target object over time
-        target.transform.rotation = Quaternion.Euler(0, Time.time * 180, 0);
+        target.transform.rotation = Quaternion.Euler(0, Time.time * 360 * spinsPerSecond, 0);
 
         // Bob the target object up and down over time
-        target.transform.position = new Vector3(objectLoc.x, objectLoc.y + bobHeight * Mathf.Sin(Time.time * spinsPerSecond * 2 * Mathf.PI), objectLoc.z);
+        target.transform.position = new Vector3(objectLoc.x, objectLoc.y + bobHeight * Mathf.Sin(Time.time * 2 * Mathf.PI), objectLoc.z);
     }
 }
