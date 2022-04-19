61dba88ff1642ba9bfa921def4f3a468cf87692e
Adam-E-B <61750702+Adam-E-B@users.noreply.github.com>
2022-04-19 16:31:45 -0400

    Add "Exit to Title" button to pause menu
    The pause menu now includes an "Exit to Title" button that takes players back to the title screen. The pause menu can now be summoned alternatively with 'Escape' in addition to 'P'.

--- CapstoneGame/Assets/Vuk's Assets/Scripts/PauseMenuManagerScript.cs
+++ CapstoneGame/Assets/Vuk's Assets/Scripts/PauseMenuManagerScript.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using FMODUnity;
 
 public class PauseMenuManagerScript : MonoBehaviour
 {
@@ -8,15 +10,20 @@ public class PauseMenuManagerScript : MonoBehaviour
 
     public GameObject GameMapUI;
 
+    FMOD.Studio.Bus MasterBus;
+
     void Awake()
     {
+        // Get the master audio bus
+        MasterBus = RuntimeManager.GetBus("Bus:/");
+
         Resume();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
         {
             if (GameIsPaused)
             {
@@ -41,5 +48,14 @@ public class PauseMenuManagerScript : MonoBehaviour
         GameMapUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+    }
+
+    public void ExitToTitle()
+    {
+        //Stop playing music
+        MasterBus.stopAllEvents(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+
+        // Go to title screen
+        SceneManager.LoadScene(0);
     }
 }
